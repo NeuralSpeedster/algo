b1f2a6d59918d198dd37f57222640da43bab2af6
DanielS <newds57066@gmail.com>
2025-08-28 19:43:01 +0300

    Added gitignore

---
+++ cpp/stack_queue/problems/correct_brackets.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Задача A. ПСП.
+// S ::= ∅ | (S) | [S] | {S} | S1 S2
+
+int main() {
+    istream::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string sequence;
+    cin >> sequence;
+    stack<char> brackets;
+
+    for (auto c : sequence) {
+        if (c == '{' || c == '[' || c == '(') {
+            brackets.push(c);
+        }
+        else if (c == '}' || c == ']' || c == ')') {
+            if (!brackets.empty()) {
+                char top = brackets.top();
+                char correct;
+                if (top == '{') {
+                    correct = '}';
+                }
+                else if (top == '(') {
+                    correct = ')';
+                }
+                else if (top == '[') {
+                    correct = ']';
+                }
+
+                if (c != correct) {
+                    cout << "no";
+                    return 0;
+                }
+                brackets.pop();
+            }
+            else {
+                cout << "no";
+                return 0;
+            }
+        }
+    }
+    if (brackets.empty()) {
+        cout << "yes";
+    }
+    else {
+        cout << "no";
+    }
+    return 0;
+}
+
