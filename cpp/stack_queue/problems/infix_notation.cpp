e8f7cb6123df32ca72a6c91936d4cad438b813e4
DanielS <newds57066@gmail.com>
2025-08-30 21:20:06 +0300

    добавил шаблон для инфиксной нотации

---
+++ cpp/stack_queue/problems/infix_notation.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int evaluate_from_prn(const string &s) {
+
+    return 0;
+}
+
+
+int main() {
+    istream::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string expression;
+    getline(cin, expression);
+    stack<int> st;
+    return 0;
+}
