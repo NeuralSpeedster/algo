f15ce4801055c1c814f37eb51f58e3d56d506d21
DanielS <newds57066@gmail.com>
2025-09-02 13:28:32 +0300

    исправлено добавление операндов

--- cpp/stack_queue/problems/infix_notation.cpp
+++ cpp/stack_queue/problems/infix_notation.cpp
@@ -6,8 +6,6 @@ int evaluate_from_pn(const string &s) {
     return 0;
 }
 
-using Element = std::variant<int, char>;
-
 unordered_map<char,  unsigned short> OPERATIONS_PRIOR = {
     {'+', 1},
     {'-', 1},
@@ -15,16 +13,15 @@ unordered_map<char,  unsigned short> OPERATIONS_PRIOR = {
     //{'(', 0},
 };
 
-string convert_to_pn(const string &s) {
-    stack<Element> st;
+string convert_to_prn(const string &s) {
+    stack<char> st;
     string res;
-    res = "1";
 
     for (unsigned int i = 0; i < s.length(); i++) {
         if (s.at(i) == ' ') {
             continue;
         }
-        // 1. Операнд сразу попадает в стек
+        // 1. Операнд сразу попадает в ответ
         // находим операнд
         unsigned int j = 0;
         string operand;
@@ -36,7 +33,7 @@ string convert_to_pn(const string &s) {
         if (j > 0) {
             i += j - 1;
             cout<<operand<<" ";
-            st.emplace(stoi(operand));
+            res.append(operand + " ");
             continue;
             /* цикл остановился либо в конце строки, либо на первом символе,
             не являющимся цифрой, тогда (инклюзивно) s[i, i+j] = operand
@@ -46,6 +43,7 @@ string convert_to_pn(const string &s) {
         //
 
     }
+    cout<<"\nRes: "<<res<<"\n";
     return res;
 }
 
@@ -56,6 +54,6 @@ int main() {
     string expression = " 12 + 456";
     //getline(cin, expression);
 
-    string result_postfix = convert_to_pn(expression);
+    string result_postfix = convert_to_prn(expression);
     return evaluate_from_pn(result_postfix);
 }
