7f556d2b09b5705952a300d8618a54ba1c29ee27
DanielS <newds57066@gmail.com>
2025-09-13 21:01:52 +0300

    Односвязный список: добавление в начало и конец, вывод на экран, удаление из начала, втавка в середину

---
+++ python/linked_list/singly_linked_list.py
@@ -0,0 +1,82 @@
+class SingleListNode:
+    def __init__(self, value):
+        self.value = value
+        self.next: [SingleListNode, None] = None
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.__head: [SingleListNode, None] = None
+        self.__tail: [SingleListNode, None] = None
+        self.__size: int = 0
+
+    def push_back(self, value):
+        new_node = SingleListNode(value)
+
+        if not self.__head:
+            self.__head = new_node
+        else:
+            self.__tail.next = new_node
+
+        self.__tail = new_node
+        self.__size += 1
+
+    def push_front(self, value):
+        new_node = SingleListNode(value)
+        if not self.__tail:
+            self.__tail = new_node
+        else:
+            new_node.next = self.__head
+
+        self.__head = new_node
+
+    def pop_front(self):
+        pass
+        self.__size += 1
+
+    def print_elements(self):
+        """
+        Это не Python стиль, но я должен показать алгоритмический смысл
+        """
+        current = self.__head
+        print("{", end="")
+        while current:
+            ending = ", " if current.next else ""
+            print(current.value, end=ending)
+            current = current.next
+        print("}")
+
+    def insert(self, index: int, value):
+        if not (0 <= index < self.__size):
+            raise IndexError("Ошибка: индекс вне диапазона")
+
+        if index == 0:
+            self.push_front(value)
+            return
+        if index == self.__size - 1:
+            self.push_back(value)
+            return
+
+        new_node = SingleListNode(value)
+        current = self.__head
+        for _ in range(index):
+            current = current.next
+
+        new_node.next = current.next
+        current.next = new_node
+        self.__size += 1
+
+    def remove(self, index: int):
+        if not (0 <= index < self.__size):
+            raise IndexError("Ошибка: индекс вне диапазона")
+
+        if index == 0:
+            self.pop_front()
+            return
+        current = self.__head
+        for _ in range(index-1):
+            current = current.next
+
+        current.next = current.next.next
+        self.__size -= 1
+
